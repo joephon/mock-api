69bcbefa6f9ed74d84b129f596bae16030da18d7
joephon <306519779@qq.com>
2019-04-24 14:56:43 +0800

    added something

--- src/api/weapp/SubjectController.js
+++ src/api/weapp/SubjectController.js
@@ -13,24 +13,32 @@ export default class SubjectController extends Extend {
    * @api {get} /weapp/subject 获取项目列表
    * @apiGroup SUBJECT
    * @apiHeader {String} Authorization token信息
-   * @apiParam {Object} [criteria={type:1}] 查询条件
-   * @apiParam {Object} [options={}] 可选项
-   * @apiParam {Object} [sort={createdAt:-1}] 排序
+   * @apiParam {Number} [type=1] 查询条件 1=报名 2=团购
+   * @apiParam {Number} [status=1] 可选项 1=接龙中 2=已截止
+   * @apiParam {Object} [sort={createdAt:-1}] 排序 默认倒序
    * @apiParam {Number} [page=1] 页码
    * @apiParam {Number} [pageSize=20] 分页长度
    */
   @router.get('/')
   @list
   async getSubjects(ctx) {
-    const { criteria, options, page, pageSize, sort } = ctx.request.query
+    const { type, status, page, pageSize, sort } = ctx.request.query
 
     return super.success({
       list: [
         {
-          title: 'test',
+          imgList: ['https://ooxx.png'],
           type: 1,
-          description: 'des'
-        }
+          status: 1,
+          description: 'des',
+          autoStop: true,
+          extraInfo: true,
+          endTime: 0,
+          limit: 20,
+          phoneNumber: '13542422222',
+          realName: 'Joephoh',
+          mail: 'joephon@qq.com'
+        }     
       ],
       total: 20,
       next: 2
