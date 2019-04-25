e539c051e82e21af23fd6d445dc8c50e60a6b020
joephon <306519779@qq.com>
2019-04-25 21:03:39 +0800

    added some logic

--- src/api/weapp/SubjectController.js
+++ src/api/weapp/SubjectController.js
@@ -21,19 +21,20 @@ const obj = {
 }; 
 
 /**
- * @apiDefine SUBJECT 项目
- * 项目结构
+ * @apiDefine SUBJECT 接龙
+ * 接龙结构
  */
 @router({ prefix: '/weapp/subject' })
 export default class SubjectController extends Extend {
   /**
    *
-   * @api {get} /weapp/subject 获取项目列表
+   * @api {get} /weapp/subject 获取接龙列表
    * @apiGroup SUBJECT
    * @apiHeader {String} Authorization token信息
+   * @apiParam {Number} [belongs=1] 查询条件 1=我发布的 2=我参与的
    * @apiParam {Number} [type=1] 查询条件 1=报名 2=团购
    * @apiParam {Number} [status=1] 可选项 1=接龙中 2=已截止
-   * @apiParam {Object} [sort={createdAt:-1}] 排序 默认倒序
+   * @apiParam {Number} [sort=-1] 排序 默认倒序
    * @apiParam {Number} [page=1] 页码
    * @apiParam {Number} [pageSize=20] 分页长度
    */
@@ -51,23 +52,22 @@ export default class SubjectController extends Extend {
 
   /**
    *
-   * @api {post} /weapp/subject 创建项目
+   * @api {post} /weapp/subject 创建接龙
    * @apiGroup SUBJECT
    * @apiHeader {String} Authorization token 信息
-   * @apiParam {String} description 描述
-   * @apiParam {Number} [type=1] 项目类型 默认1=报名 2=团购
+   * @apiParam {Number} [type=1] 接龙类型 默认1=报名 2=团购
    * @apiParam {String[]} [imgList] 图片列表
-   * @apiParam {Object[]} [goodsList] 商品类型
+   * @apiParam {Object[]} [goodsList] 商品列表
    * @apiParam {Number} [status=1] 状态
    * @apiParam {Number} [limit=0] 截止人数 不传默认无限
    * @apiParam {Boolean} [autoStop=false] 自动截止
    * @apiParam {Boolean} [needPhoneNumber=false] 报名需要填手机
    * @apiParam {Boolean} [needRealName=false] 报名需要填收货人姓名
    * @apiParam {Boolean} [needEmail=false] 报名需要填写邮箱
-   * @apiParam {String} description 项目描述
+   * @apiParam {String} description 接龙描述
    */
   @router.post('/')
-  @required({ body: { description: String }, type: Number })
+  @required({ body: { description: String } })
   async createSubject(ctx) {
     const { id } = ctx.decoded;
 
@@ -75,5 +75,54 @@ export default class SubjectController extends Extend {
       createdBy: id,
       ...obj
     };
+
+    return super.success(subject);
+  }
+
+  /**
+   *
+   * @api {put} /weapp/subject/:id 编辑接龙
+   * @apiGroup SUBJECT
+   * @apiHeader {String} Authorization token 信息
+   * @apiParam {String} description 描述
+   * @apiParam {String[]} [imgList] 图片列表
+   * @apiParam {Object[]} [goodsList] 商品列表
+   * @apiParam {Number} [status=1] 状态
+   * @apiParam {Number} [limit=0] 截止人数 不传默认无限
+   * @apiParam {Boolean} [autoStop=false] 自动截止
+   * @apiParam {Boolean} [needPhoneNumber=false] 报名需要填手机
+   * @apiParam {Boolean} [needRealName=false] 报名需要填收货人姓名
+   * @apiParam {Boolean} [needEmail=false] 报名需要填写邮箱
+   * @apiParam {String} description 接龙描述
+   * @apiParam {String} id 目标接龙id
+   */
+  @router.put('/:id')
+  @required({ body: { description: String }, params: { id: String } })
+  async editSubject(ctx) {
+    const { id } = ctx.decoded;
+
+    const subject = {
+      createdBy: id,
+      ...obj
+    };
+
+    return super.success(subject)
+  }
+
+  /**
+   *
+   * @api {delete} /weapp/subject/:id 删除接龙
+   * @apiGroup SUBJECT
+   * @apiHeader {String} Authorization token 信息
+   * @apiParam {String} id 目标接龙id
+   */
+  @router.put('/:id')
+  @required({ params: { id: String } })
+  async deleteSubject(ctx) {
+    const { id } = ctx.decoded;
+
+    return super.success();
   }
 }
+
+
